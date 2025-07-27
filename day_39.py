7006b649c74915604711c5373cb21b421d3c7619
gaboToro <104457588+gaboToro@users.noreply.github.com>
2025-07-26 23:47:56 -0500

    The hangman game

---
+++ day_39.py
@@ -0,0 +1,42 @@
+import os, time
+
+word = input('Enter a word: ').upper()
+letterPicked = []
+lives = 6
+
+while True:
+  time.sleep(1)
+  os.system('cls')
+  letter = input("Choose a letter: ").upper()
+  
+  if letter in letterPicked:
+    print("You've tried that before")
+    continue
+    
+  letterPicked.append(letter)
+  
+  if letter in word:
+    print("You found a letter")
+  else:
+    print("Nope, not in there")
+    lives -= 1
+  
+  allLetters = True
+  print()
+  for i in word:
+    if i in letterPicked:
+      print(i, end="")
+    else:
+      print("_", end="")
+      allLetters = False
+  print()
+
+  if allLetters:
+    print(f"You won with {lives} left!")
+    break
+
+  if lives<=0:
+    print(f"You ran out of lives! The answer was {word}")
+    break
+  else:
+    print(f"Only {lives} left")
